381f787b12ce882529b9006d6e66594873f224c1
cjhosken <hoskenchristopher@gmail.com>
2024-01-30 10:55:56 +0000

    menu setup

--- MFS_menu.py
+++ MFS_menu.py
@@ -1,15 +1,10 @@
 from maya import cmds
 
 def create_custom_menu():
-    # Check if the menu already exists, and delete it if it does
-    if cmds.menu("customMenu", exists=True):
-        cmds.deleteUI("customMenu", menu=True)
+    if cmds.menu("MFS_menu", exists=True):
+        cmds.deleteUI("MFS_menu", menu=True)
 
-    # Create the menu
-    custom_menu = cmds.menu("customMenu", label="Custom Menu", parent="MayaWindow", tearOff=True)
+    MFS_menu = cmds.menu("MFS_menu", label="Maya Fluid Simulator", parent="MayaWindow", tearOff=False)
 
-    # Add items to the menu
-    cmds.menuItem(label="Item 1", command="print('Item 1 selected')")
-    cmds.menuItem(label="Item 2", command="print('Item 2 selected')")
-    cmds.menuItem(divider=True)
-    cmds.menuItem(label="Item 3", command="print('Item 3 selected')")
+    cmds.menuItem(label="Domain", command="print('domain applied')")
+    cmds.menuItem(label="Solver", command="print('solver applied')")
