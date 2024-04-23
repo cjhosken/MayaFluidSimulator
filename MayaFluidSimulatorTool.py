c1492750feea85f5ec010a1beadbc7490b0158fe
cjhosken <hoskenchristopher@gmail.com>
2024-04-23 14:30:29 +0100

    flip/pic mixing

--- MayaFluidSimulatorTool.py
+++ MayaFluidSimulatorTool.py
@@ -54,10 +54,13 @@ class MFS_Plugin():
         self.density_ctrl = cmds.floatSliderGrp(minValue=0, maxValue=2000, step=0.01, value=998.2, field=True, label="Fluid Density")
         self.visc_ctrl = cmds.floatSliderGrp(minValue=0, step=0.01, value=0.1, field=True, label="Viscosity Factor")
         self.damp_ctrl = cmds.floatSliderGrp(minValue=0, step=0.01, value=0.9, maxValue=1, field=True, label="Floor Damping")
+
+        self.picflip_ctrl = cmds.floatSliderGrp(minValue=0, maxValue=1.0, step=0.01, value=0.5, field=True, label="PIC/FLIP Mix")
         
-        cmds.rowLayout(numberOfColumns=3)
+        cmds.rowLayout(numberOfColumns=2)
         self.time_ctrl = cmds.intFieldGrp(numberOfFields=2, value1=0, value2=120, label="Frame Range")
         self.ts_ctrl = cmds.floatSliderGrp(minValue=0, step=0.01, value=0.1, field=True, label="Time Scale")
+        
 
         solve_row = cmds.rowLayout(numberOfColumns=2, parent=simulate_section, adjustableColumn = True)
 
@@ -140,6 +143,7 @@ class MFS_Plugin():
             viscosity_factor = cmds.floatSliderGrp(self.visc_ctrl, query=True, value=True)
             damping =  (1 - cmds.floatSliderGrp(self.damp_ctrl, query=True, value=True))
             pscale = cmds.floatSliderGrp(self.pscale_ctrl, query=True, value=True)
+            flipFac = cmds.floatSliderGrp(self.picflip_ctrl, query=True, value=True)
 
             particles = cmds.listRelatives(source + "_particles", children=True) or []
             points = []
@@ -172,10 +176,10 @@ class MFS_Plugin():
             grid = MFS_Grid(resolution, cell_size)
 
             cmds.progressWindow(title='Simulating', progress=0, status='Progress: 0%', isInterruptable=True, maxValue=(frame_range[1]-frame_range[0]))
-            self.update(source, points, grid, frame_range, timescale, external_force, fluid_density, viscosity_factor, damping, pscale, 0)
+            self.update(source, points, grid, frame_range, timescale, external_force, fluid_density, viscosity_factor, damping, pscale, flipFac, 0)
 
 
-    def update(self, source, points, grid, frame_range, timescale, external_force, fluid_density, viscosity_factor, damping, pscale, progress):
+    def update(self, source, points, grid, frame_range, timescale, external_force, fluid_density, viscosity_factor, damping, pscale, flipFac, progress):
         percent = (progress / (frame_range[1] - frame_range[0])) * 100
 
         cmds.progressWindow(e=1, progress=progress, status=f'Progress: {percent:.1f}%')
@@ -191,15 +195,15 @@ class MFS_Plugin():
 
             while (cfl_time < timescale):
                 timestep = grid.from_particles(source, points, timescale)
-                grid.calc_forces(external_force, viscosity_factor, timestep)
+                grid.calc_forces(external_force, timestep)
                 grid.enforce_boundaries()
                 grid.solve_poisson()
-                grid.to_particles(source, points, timestep, damping)
+                grid.to_particles(source, points, timestep, damping, flipFac)
                 cfl_time += timestep
             
             cmds.currentTime(t + 1, edit=True)
 
-            self.update(source, points, grid, frame_range, timescale, external_force, fluid_density, viscosity_factor, damping, pscale, progress=progress+1)
+            self.update(source, points, grid, frame_range, timescale, external_force, fluid_density, viscosity_factor, damping, pscale, flipFac, progress=progress+1)
         else:
             cmds.currentTime(frame_range[0], edit=True)
             cmds.progressWindow(endProgress=1)
@@ -342,13 +346,17 @@ class MFS_Particle():
         self.position = pos
         self.velocity = vel
 
-    def advect(self, source, velocity, damping, dt):
+    def advect(self, source, velocity, velocity_change, damping, dt, flipFac):
         bbox = cmds.exactWorldBoundingBox(source + "_domain")
         min_x, min_y, min_z, max_x, max_y, max_z = bbox
 
-        print("PVEL: ", velocity)
+        flipFac = max(min(flipFac, 1.0), 0.0)
 
-        self.velocity = velocity
+
+        pic_vel = velocity
+        flip_vel = self.velocity + velocity_change
+
+        self.velocity = (flipFac) * flip_vel + (1-flipFac) * pic_vel
 
         advected = self.position + self.velocity
 
@@ -378,6 +386,7 @@ class MFS_Grid():
         self.cell_size = cell_size
 
         self.velocity = np.zeros((self.resolution[0] + 1, self.resolution[1] + 1, self.resolution[2] + 1, 3), dtype="float64")
+        self.last_velocity = np.zeros((self.resolution[0] + 1, self.resolution[1] + 1, self.resolution[2] + 1, 3), dtype="float64")
         self.type = np.zeros((self.resolution[0], self.resolution[1], self.resolution[2], 3), dtype="float64")
 
     def from_particles(self, source, points, timescale):
@@ -403,6 +412,11 @@ class MFS_Grid():
             dy = y - j
             dz = z - k
 
+            # Clamp indices to stay within the grid boundaries
+            i = max(0, min(i, self.resolution[0] - 1))
+            j = max(0, min(j, self.resolution[1] - 1))
+            k = max(0, min(k, self.resolution[2] - 1))
+
             self.type[i][j][k] = 1
 
             # Trilinear interpolation for velocity components
@@ -414,8 +428,6 @@ class MFS_Grid():
             w101 = dx * (1 - dy) * dz
             w011 = (1 - dx) * dy * dz
             w111 = dx * dy * dz
-
-            print("SUM: ", dx + dy + dz)
 
             # Update velocity grid using trilinear interpolation
             self.velocity[i][j][k] += point.velocity * w000
@@ -442,23 +454,23 @@ class MFS_Grid():
             self.velocity[min(i + 1, self.resolution[0] - 1)][min(j + 1, self.resolution[1] - 1)][min(k + 1, self.resolution[2] - 1)] += point.velocity * w111
             weights[min(i + 1, self.resolution[0] - 1)][min(j + 1, self.resolution[1] - 1)][min(k + 1, self.resolution[2] - 1)] += w111
 
-            print("VEL1: ", self.velocity[i][j][k])
-
         max_vel = 0.001
 
         # Calculate timestep based on maximum velocity
         for i in range(self.resolution[0]):
             for j in range(self.resolution[1]):
                 for k in range(self.resolution[2]):
-                    self.velocity[i][j][k] /= weights[i][j][k]
+                    if (weights[i][j][k] != 0):
+                        self.velocity[i][j][k] /= weights[i][j][k]
                     max_vel = max(max_vel, np.linalg.norm(self.velocity[i][j][k]))
 
         timestep = timescale * min(np.linalg.norm(self.cell_size) / max_vel, 1)
 
+        self.last_velocity = np.array(self.velocity, copy=True)
+
         return timestep
 
-
-    def calc_forces(self, external_force, viscosity, dt):
+    def calc_forces(self, external_force, dt):
         for i in range(self.resolution[0]):
             for j in range(self.resolution[1]):
                 for k in range(self.resolution[2]):
@@ -473,7 +485,7 @@ class MFS_Grid():
         #TODO: Solve the poisson equation to make the fluid non-divergent.
         pass
 
-    def to_particles(self, source, points, dt, damping):
+    def to_particles(self, source, points, dt, damping, flipFac):
         bbox = cmds.exactWorldBoundingBox(source + "_domain")
         min_x, min_y, min_z, max_x, max_y, max_z = bbox
 
@@ -481,6 +493,10 @@ class MFS_Grid():
             x = (point.position[0] - min_x) / self.cell_size[0]
             y = (point.position[1] - min_y) / self.cell_size[1]
             z = (point.position[2] - min_z) / self.cell_size[2]
+
+            last_x = x - point.velocity[0] * dt / self.cell_size[0]
+            last_y = y - point.velocity[1] * dt / self.cell_size[1]
+            last_z = z - point.velocity[2] * dt / self.cell_size[2]
 
             #TODO: FLIP / PIC METHOD
             # FLIP interpolates the change of velocity and adds it to the existing velocity
@@ -490,8 +506,12 @@ class MFS_Grid():
 
             #TODO: The particles need to get the trillinearly interpolated velocity from the grid.
 
-            velocity = self.trilinear_interpolate_velocity(x, y, z)
-            point.advect(source, velocity, damping, dt)
+            velocity, vw = self.trilinear_interpolate_velocity(x, y, z)
+            last_velocity, lvw = self.trilinear_interpolate_last_velocity(last_x, last_y, last_z)
+            
+            velocity_change = velocity/vw - last_velocity/lvw
+
+            point.advect(source, velocity/vw, velocity_change, damping, dt, flipFac)
 
     def trilinear_interpolate_velocity(self, x, y, z):
         i = int(x)
@@ -506,39 +526,76 @@ class MFS_Grid():
         j = max(0, min(j, self.resolution[1] - 1))
         k = max(0, min(k, self.resolution[2] - 1))
 
-        # Trilinear interpolation for u component
-        v000 = self.velocity[i][j][k]
-        v100 = self.velocity[min(i + 1, self.resolution[0] - 1)][j][k]
-        v010 = self.velocity[i][min(j + 1, self.resolution[1] - 1)][k]
-        v110 = self.velocity[min(i + 1, self.resolution[0] - 1)][min(j + 1, self.resolution[1] - 1)][k]
-        v001 = self.velocity[i][j][min(k + 1, self.resolution[2] - 1)]
-        v101 = self.velocity[min(i + 1, self.resolution[0] - 1)][j][min(k + 1, self.resolution[2] - 1)]
-        v011 = self.velocity[i][min(j + 1, self.resolution[1] - 1)][min(k + 1, self.resolution[2] - 1)]
-        v111 = self.velocity[min(i + 1, self.resolution[0] - 1)][min(j + 1, self.resolution[1] - 1)][min(k + 1, self.resolution[2] - 1)]
+        w000 = (1 - dx) * (1 - dy) * (1 - dz)
+        w100 = dx * (1 - dy) * (1 - dz)
+        w010 = (1 - dx) * dy * (1 - dz)
+        w110 = dx * dy * (1 - dz)
+        w001 = (1 - dx) * (1 - dy) * dz
+        w101 = dx * (1 - dy) * dz
+        w011 = (1 - dx) * dy * dz
+        w111 = dx * dy * dz
+
+        velocity = (
+            self.velocity[i][j][k] * w000 + 
+            self.velocity[min(i + 1, self.resolution[0] - 1)][j][k] * w100 +
+            self.velocity[i][min(j + 1, self.resolution[1] - 1)][k] * w010 +
+            self.velocity[min(i + 1, self.resolution[0] - 1)][min(j + 1, self.resolution[1] - 1)][k] * w110 +
+            self.velocity[i][j][min(k + 1, self.resolution[2] - 1)] * w001 +
+            self.velocity[min(i + 1, self.resolution[0] - 1)][j][min(k + 1, self.resolution[2] - 1)] * w101 +
+            self.velocity[i][min(j + 1, self.resolution[1] - 1)][min(k + 1, self.resolution[2] - 1)] * w011 +
+            self.velocity[min(i + 1, self.resolution[0] - 1)][min(j + 1, self.resolution[1] - 1)][min(k + 1, self.resolution[2] - 1)] * w111
+        )
+
+        weight = w000 + w100 + w010 + w110 + w001 + w101 + w011 + w111
 
         # Trilinear interpolation for v and w components and interpolate them similarly
 
         # Return the interpolated velocity
-        return trilinear_interpolate(v000, v100, v010, v110, v001, v101, v011, v111, dx, dy, dz)
-  
+        return (velocity, weight)
+    
+    def trilinear_interpolate_last_velocity(self, x, y, z):
+        i = int(x)
+        j = int(y)
+        k = int(z)
 
+        dx = x - i
+        dy = y - j
+        dz = z - k
+
+        # Clamp indices to stay within the grid boundaries
+        i = max(0, min(i, self.resolution[0] - 1))
+        j = max(0, min(j, self.resolution[1] - 1))
+        k = max(0, min(k, self.resolution[2] - 1))
+
+        w000 = (1 - dx) * (1 - dy) * (1 - dz)
+        w100 = dx * (1 - dy) * (1 - dz)
+        w010 = (1 - dx) * dy * (1 - dz)
+        w110 = dx * dy * (1 - dz)
+        w001 = (1 - dx) * (1 - dy) * dz
+        w101 = dx * (1 - dy) * dz
+        w011 = (1 - dx) * dy * dz
+        w111 = dx * dy * dz
+
+        velocity = (
+            self.last_velocity[i][j][k] * w000 + 
+            self.last_velocity[min(i + 1, self.resolution[0] - 1)][j][k] * w100 +
+            self.last_velocity[i][min(j + 1, self.resolution[1] - 1)][k] * w010 +
+            self.last_velocity[min(i + 1, self.resolution[0] - 1)][min(j + 1, self.resolution[1] - 1)][k] * w110 +
+            self.last_velocity[i][j][min(k + 1, self.resolution[2] - 1)] * w001 +
+            self.last_velocity[min(i + 1, self.resolution[0] - 1)][j][min(k + 1, self.resolution[2] - 1)] * w101 +
+            self.last_velocity[i][min(j + 1, self.resolution[1] - 1)][min(k + 1, self.resolution[2] - 1)] * w011 +
+            self.last_velocity[min(i + 1, self.resolution[0] - 1)][min(j + 1, self.resolution[1] - 1)][min(k + 1, self.resolution[2] - 1)] * w111
+        )
+
+        weight = w000 + w100 + w010 + w110 + w001 + w101 + w011 + w111
+
+        # Trilinear interpolation for v and w components and interpolate them similarly
+
+        # Return the interpolated velocity
+        return (velocity, weight)
+  
     def clear(self):
         self.velocity = np.zeros((self.resolution[0] + 1, self.resolution[1] + 1, self.resolution[2] + 1, 3), dtype="float64")
-
-def linear_interpolate(p0, p1, t):
-    weight = 1 - t if t > 0.5 else t
-    return p0 * (1 - weight) + p1 * weight
-
-def bilinear_interpolate(p00, p10, p01, p11, x, y):
-    v0 = linear_interpolate(p00, p10, x)
-    v1 = linear_interpolate(p01, p11, x)
-
-    return linear_interpolate(v0, v1, y)
-
-def trilinear_interpolate(p000, p100, p010, p110, p001, p101, p011, p111, x, y, z):
-    v00 = bilinear_interpolate(p000, p100, p010, p110, x, y)
-    v01 = bilinear_interpolate(p001, p101, p011, p111, x, y)
-    return linear_interpolate(v00, v01, z)
 
 # Create and initialize the plugin.
 if __name__ == "__main__":
